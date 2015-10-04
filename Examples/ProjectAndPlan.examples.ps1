b1a6d0ea30415802d8e192649eefa001721feb8c
Akos Murati <akos.murati@xero.com>
2015-10-04 17:52:48 +1300

    Restructure examples

--- Examples/ProjectAndPlan.examples.ps1
+++ Examples/ProjectAndPlan.examples.ps1
@@ -1,62 +1,71 @@
 Import-Module PsBamboo
 
-$Server = 'http://localhost:8085'
-$UserName = 'testuser'
-$Password = 'testpassword'
+#region Project Demo
+    $Server = 'http://localhost:8085'
+    $UserName = 'testuser'
+    $Password = 'testpassword'
 
-Write-Host "Bamboo Server and Login Credentials" -ForegroundColor Cyan
-Set-BambooServer -Url $Server
-Set-BambooCredential -UserName $UserName -Password $Password
-Get-BambooCurrentUser | Format-List
+    Write-Host "Set Bamboo Server and Login Credentials" -ForegroundColor Cyan
+    Set-BambooServer -Url $Server
+    Set-BambooCredential -UserName $UserName -Password $Password
+    Get-BambooCurrentUser | Format-List
+#endregion
 
+#region Project Demo
+    Read-Host "Get all Projects - Press ENTER"
+    $AllProjects = Get-BambooProject
+    $AllProjects | Out-Host
 
-Write-Host "Enumerate and Detail Projects" -ForegroundColor Cyan
-# Get all Projects
-$AllProjects = Get-BambooProject
-$AllProjects
+    Read-Host "Detail a specific Project - Press ENTER"
+    $First = $AllProjects | Select -First 1 -ExpandProperty key
+    $FirstProject = Get-BambooProject -ProjectKey $First
+    $FirstProject | Format-List
+#endregion
 
-# Get details for a single projects
-$First = $AllProjects | Select -First 1 -ExpandProperty key
-$FirstProject = Get-BambooProject -ProjectKey $First
-$FirstProject | Format-List
+#region Plan Demo
 
-
-Write-Host "List Plans" -ForegroundColor Cyan
-$Plans = Get-BambooPlan
-$Plans | Format-Table Key,Name
-
-
-Write-Host "Detail and Manipulate a Plan" -ForegroundColor Cyan
-$FirstPlan = $Plans | Select -First 1 -ExpandProperty key
-Get-BambooPlan -PlanKey $FirstPlan
-
-# Disable Plan and show details
-Disable-BambooPlan -PlanKey $FirstPlan
-Get-BambooPlan -PlanKey $FirstPlan | Format-Table key,name,enabled
-
-# Re-enable Plan and show details
-Enable-BambooPlan -PlanKey $FirstPlan
-Get-BambooPlan -PlanKey $FirstPlan | Format-Table key,name,enabled
-
-# Copy (Clone) a Plan
-$NewPlanKey = "$($FirstPlan)2"
-Copy-BambooPlan -PlanKey $FirstPlan -NewPlanKey $NewPlanKey
+    Read-Host "List Plans - Press ENTER"
+    $Plans = Get-BambooPlan
+    $Plans | Format-Table Key,Name
 
 
-Write-Host "Manipulate PlanBranches" -ForegroundColor Cyan
-# Add a new PlanBranch
-$BranchName='pester'
-$VcsBranch='feature/pester'
-$NewBranchPlan = Add-BambooPlanBranch -PlanKey $NewPlanKey -BranchName $BranchName -VcsBranch $VcsBranch
-$NewBranchPlan
+    Read-Host "Detail a specific Plan - Press ENTER"
+    $FirstPlan = $Plans | Select -First 1 -ExpandProperty key
+    Get-BambooPlan -PlanKey $FirstPlan | Out-Host
 
-# Enable PlanBranch
-Enable-BambooPlanBranch -PlanKey $NewBranchPlan.key
-Get-BambooPlanBranch -PlanKey $NewBranchPlan.key | Format-Table key,name,enabled
+    Read-Host "Disable Plan and show details - Press ENTER"
+    Disable-BambooPlan -PlanKey $FirstPlan
+    Get-BambooPlan -PlanKey $FirstPlan | Format-Table key,name,enabled
 
-# Disable PlanBranch
-Disable-BambooPlanBranch -PlanKey $NewBranchPlan.key
-Get-BambooPlanBranch -PlanKey $NewBranchPlan.key | Format-Table key,name,enabled
 
-# Remove the new Plan Branch
-Remove-BambooPlanBranch -PlanKey $NewBranchPlan.key
+    Read-Host "Re-enable Plan and show details - Press ENTER"
+    Enable-BambooPlan -PlanKey $FirstPlan
+    Get-BambooPlan -PlanKey $FirstPlan | Format-Table key,name,enabled
+
+    Read-Host "Copy (Clone) a Plan - Press Enter"
+    $NewPlanKey = "$($FirstPlan)2"
+    Copy-BambooPlan -PlanKey $FirstPlan -NewPlanKey $NewPlanKey
+
+#endregion
+
+#region PlanBranch demo
+
+    Write-Host "Manipulate PlanBranches" -ForegroundColor Cyan
+    
+    Read-Host "Add a new PlanBranch - Press ENTER"
+    $BranchName='pester'
+    $VcsBranch='feature/pester'
+    $NewBranchPlan = Add-BambooPlanBranch -PlanKey $NewPlanKey -BranchName $BranchName -VcsBranch $VcsBranch
+    $NewBranchPlan
+
+    Read-Host "Enable PlanBranch - Press ENTER"
+    Enable-BambooPlanBranch -PlanKey $NewBranchPlan.key
+    Get-BambooPlanBranch -PlanKey $NewBranchPlan.key | Format-Table key,name,enabled
+
+    Read-Host "Disable PlanBranch - Press ENTER"
+    Disable-BambooPlanBranch -PlanKey $NewBranchPlan.key
+    Get-BambooPlanBranch -PlanKey $NewBranchPlan.key | Format-Table key,name,enabled
+
+    Read-Host "Remove the new PlanBranch - Press ENTER"
+    Remove-BambooPlanBranch -PlanKey $NewBranchPlan.key
+#endregion
