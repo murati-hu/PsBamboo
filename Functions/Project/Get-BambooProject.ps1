e86b3ad347eb5799d2e7c783dd396d4c2b7ff093
Akos Murati <akos.murati@xero.com>
2015-10-08 19:52:52 +1300

    Add Project ViewFormat

--- Functions/Project/Get-BambooProject.ps1
+++ Functions/Project/Get-BambooProject.ps1
@@ -23,8 +23,7 @@ function Get-BambooProject {
         $resource = "project/$ProjectKey"
     }
 
-    Invoke-BambooRestMethod -Resource $resource | #-Expand 'projects.project'
-    Expand-BambooResource -ResourceName 'project' #|
-    #Add-Member -MemberType AliasProperty ProjectKey key -PassThru |
-    #Add-Member -MemberType AliasProperty ProjectName name -PassThru
+    Invoke-BambooRestMethod -Resource $resource |
+    Expand-BambooResource -ResourceName 'project' |
+    Add-ObjectType -TypeName 'PsBamboo.Project'
 }
