e8473dc89d3416882a5eada63db32be69d9367de
Akos Murati <akos.murati@xero.com>
2015-10-08 15:54:07 +1300

    Add ViewFormat support

--- PsBamboo.psm1
+++ PsBamboo.psm1
@@ -7,6 +7,12 @@ Get-ChildItem -Path $functionFilter -Recurse | Foreach-Object {
     . $_.FullName
 }
 
+$formatFilter = Join-Path $PSScriptRoot "Functions\*.format.ps1xml"
+Get-ChildItem -Path $formatFilter -Recurse | Foreach-Object {
+    Write-Verbose "Loading format $($_.Name).."
+    Update-formatdata -PrependPath $_.FullName
+}
+
 $script:BambooServer = 'http://localhost:8085'
 $script:BambooAuthMode = $null
 $script:BambooCredential = $null
