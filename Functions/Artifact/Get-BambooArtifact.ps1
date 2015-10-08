7415b1ab6954def968c9bef04f81c54558a40b9e
Akos Murati <akos@murati.hu>
2015-10-08 16:07:21 +1300

    Add View for BambooArtifact

--- Functions/Artifact/Get-BambooArtifact.ps1
+++ Functions/Artifact/Get-BambooArtifact.ps1
@@ -2,17 +2,24 @@
 .SYNOPSIS
     Gets all the artifacts for a specific Bamboo Plan.
 .PARAMETER PlanKey
-    String for the Bamboo Plan.
+    Mandatory - Key for the Bamboo Plan.
 .EXAMPLE
     Get-BambooArtifact -PlanKey 'PRJ-PLANKEY'
+
+    id      name       shared location copyPattern PlanKey
+    --      ----       ------ -------- ----------- -------
+    1343490 test       true   /output/ *           PRJ-PLANKEY
+
 #>
 function Get-BambooArtifact {
     param(
-        [Parameter()]
+        [Parameter(Mandatory=$true)]
         [ValidatePattern('\w+-\w+')]
         [string]$PlanKey
     )
 
     Invoke-BambooRestMethod -Resource "plan/$PlanKey/artifact" |
-    Expand-BambooResource -ResourceName 'artifact'
+    Expand-BambooResource -ResourceName 'artifact' |
+    Add-Member -NotePropertyName PlanKey -NotePropertyValue $PlanKey -PassThru |
+    Add-ObjectType -TypeName 'PsBamboo.Artifact'
 }
