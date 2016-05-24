3d335caf8e679cb9750bd0c3696a3d70c6e8c0df
Akos Murati <akos@murati.hu>
2016-05-24 14:17:46 +1200

    Update README

--- Functions/BuildLog/Get-BambooBuildLog.ps1
+++ Functions/BuildLog/Get-BambooBuildLog.ps1
@@ -1,14 +1,16 @@
 <#
 .SYNOPSIS
-    Gets build logs for a single Bamboo Build Result
+    Gets build logs for a single Stage in a Bamboo Build Result
 .DESCRIPTION
-    It fetches the build result for a specific or latest build a specific
-    plan and returns it as text.
- 
+    It fetches the build result for a specific or latest build plan and
+    returns it as text.
+
 .PARAMETER BuildKey
-    Optional - Key for the exact Bamboo build to get the build results
+    Mandatory - Key for the exact Bamboo build to get the build results
+.PARAMETER StageKey
+    Mandatory - Key for the build stage
 .PARAMETER PlanKey
-    Optional - Key for the Bamboo Plans to filter 
+    Mandatory - Key for the Bamboo Plans to filter
 .PARAMETER Build
     Optional - Parameter to specify the build in question, by default it
     always returns the latest build. The valid values are:
@@ -16,10 +18,10 @@
         BUILD-Number - to return an exact build result by its id
 
 .EXAMPLE
-    Get-BambooBuildLog -BuildKey PSB-BB-22
-
+    Get-BambooBuildLog -PlanKey PSB-BB -StageKey BASE
 .EXAMPLE
-    Get-BambooBuildLog 
+    Get-BambooBuildLog -BuildKey PSB-BB-22 -StageKey BASE
+
 #>
 function Get-BambooBuildLog {
     [CmdletBinding(DefaultParameterSetName="ByPlanKey")]
@@ -37,18 +39,18 @@ function Get-BambooBuildLog {
         [Parameter(ParameterSetName="ByPlanKey")]
         [ValidatePattern('\d+|latest')]
         [string]$Build='latest',
-        
+
         [Parameter(Mandatory,ParameterSetName="ByPlanKey")]
         [Parameter(Mandatory,ParameterSetName="ByBuildKey")]
         [ValidatePattern('\w+')]
         [string]$StageKey
     )
-    
+
     if ($BuildKey -and ($BuildKey -imatch '(\w+-\w+)-(\d+)')) {
         $PlanKey = $Matches[1]
         $Build = $Matches[2]
     }
-    
+
     $buildResult = Get-BambooBuild -PlanKey $PlanKey -Build $Build
     $logUrl = "$($script:BambooServer)/download/$PlanKey-$StageKey/build_logs/$PlanKey-$StageKey-$($buildResult.buildNumber).log"
     (Invoke-WebRequest -Uri $logUrl -UseBasicParsing -ErrorAction Stop).Content
