02aa6451d28e64982d8ef1148a0fbc06f984fce8
Akos Murati <akos@murati.hu>
2018-01-06 06:16:45 +1300

    Reenable master branch restriction after successful upload

--- Deploy/PSGallery.PSDeploy.ps1
+++ Deploy/PSGallery.PSDeploy.ps1
@@ -1,5 +1,5 @@
 ﻿# Publish to gallery with a few restrictions
-if ($env:BHBuildSystem -ine 'Unknown') {  # -and $env:BHBranchName -eq "master") {
+if ($env:BHBuildSystem -ine 'Unknown' -and $env:BHBranchName -eq "master") {
     Deploy Module {
         By PSGalleryModule {
             FromSource $Env:BHPSModulePath
