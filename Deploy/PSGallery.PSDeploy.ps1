3f2e393378c6d6426afe3e71cd9c54e20fccdfae
Akos Murati <akos@murati.hu>
2018-01-06 05:58:05 +1300

    Add more visible exception

--- Deploy/PSGallery.PSDeploy.ps1
+++ Deploy/PSGallery.PSDeploy.ps1
@@ -12,5 +12,6 @@ if ($env:BHBuildSystem -ine 'Unknown' -and $env:BHBranchName -eq "master") {
 } else {
     "Skipping deployment: To deploy, ensure that...`n" +
     "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
-    "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n"
+    "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" |
+    Write-Host -ForegroundColor Yellow
 }
